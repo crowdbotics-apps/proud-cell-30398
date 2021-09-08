073255feec0b08c8d80aa9e6d2f04f21d17bcc65
Crowdbotics <team@crowdbotics.com>
2021-09-08 20:49:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_cell_30398Identifier",
+  url: "https://proud_cell_30398Identifier.botics.co",
+  api: "https://proud_cell_30398Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
